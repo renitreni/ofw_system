78e8445f38bf8dcda0d8f4cd80c5095dc0fb82c1
jemaeee17 <jemaegracezamora@gmail.com>
2026-02-02 16:08:10 +0800

    Added folders and files

---
+++ resources/js/Main.jsx
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../css/app.css';
+
+import LandingPage from './pages/LandingPage';
+
+export default function Main() {
+    return <LandingPage />
+}
---
+++ resources/js/pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "../../css/app.css";
+
+export default function LandingPage() {
+    return (
+        <h1 className='text-primary'>This is a Landing Page.</h1>
+    );
+}
